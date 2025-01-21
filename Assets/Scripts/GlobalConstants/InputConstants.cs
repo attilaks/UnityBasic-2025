e9e7851bba8d03664dd8e0c74ffb0ba333c0d39b
Скотников Кирилл Андреевич <KASkotnikov@sberbank.ru>
2025-01-21 19:27:27 +0300

    regions in InputConstants.cs

--- Assets/Scripts/GlobalConstants/InputConstants.cs
+++ Assets/Scripts/GlobalConstants/InputConstants.cs
@@ -2,8 +2,17 @@
 {
 	public static class InputConstants
 	{
+		#region Controllers
+		
 		public const string KeyBoard = "<Keyboard>";
+		
+		#endregion
+		
+		#region KeyboardKeys
+		
 		public const string SpaceKey = "space";
 		public const string FKey = "f";
+		
+		#endregion
 	}
 }
