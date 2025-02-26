801f487906c68d83f0e22d643384897ceaf90959
Скотников Кирилл Андреевич <KASkotnikov@sberbank.ru>
2025-02-26 03:23:57 +0300

    in process

---
+++ Assets/Scripts/Tools/FirstPersonCamera.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using UnityEngine;
+
+namespace Tools
+{
+	public class FirstPersonCamera : MonoBehaviour
+	{
+		[SerializeField] private float mouseSensitivity = 200f;
+		
+		[SerializeField] private Transform _playerBody;
+		private float _xRotation;
+		private float _yRotation;
+
+		private void Awake()
+		{
+			// _playerBody = gameObject.GetComponentInParent<Transform>();
+		}
+
+		private void Start()
+		{
+			Cursor.lockState = CursorLockMode.Locked;
+		}
+
+		private void Update()
+		{
+			var mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+			var mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+			if (mouseX != 0)
+			{
+				var x = 7;
+			}
+			_playerBody.Rotate(Vector3.up * mouseX);
+			
+			_xRotation -= mouseY;
+			_yRotation += mouseX;
+
+			transform.localRotation = Quaternion.Euler(_xRotation, 0f, 0f);
+			// _playerBody.Rotate(0, targetAngle, 0);
+		}
+	}
+}
---
+++ Assets/Scripts/Tools/FirstPersonMovementManager.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using GlobalConstants;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Tools
+{
+	public class FirstPersonMovementManager : MonoBehaviour
+	{
+		[SerializeField] private float movementSpeed = 2f;
+		[SerializeField] private float rotationSpeed = 2f;
+		
+		private readonly InputAction _moveForward = new("MoveForward", InputActionType.Value, 
+			$"{InputConstants.KeyBoard}/{InputConstants.W}");
+		private readonly InputAction _moveLeft = new("MoveLeft", InputActionType.Value, 
+			$"{InputConstants.KeyBoard}/{InputConstants.A}");
+		private readonly InputAction _moveRight = new("MoveRight", InputActionType.Value, 
+			$"{InputConstants.KeyBoard}/{InputConstants.D}");
+		private readonly InputAction _moveBack = new("MoveBackward", InputActionType.Value, 
+			$"{InputConstants.KeyBoard}/{InputConstants.S}");
+
+		private readonly InputAction _rotateLeft = new("RotateLeft", InputActionType.Value, 
+			$"{InputConstants.KeyBoard}/{InputConstants.Q}");
+		private readonly InputAction _rotateRight = new("RotateRight", InputActionType.Value, 
+			$"{InputConstants.KeyBoard}/{InputConstants.E}");
+
+		#region Monobehaviour methods
+
+		private void Update()
+		{
+			if (_moveForward.IsPressed())
+			{
+				MoveForwardByTransform();
+			}
+			if (_moveLeft.IsPressed())
+			{
+				MoveLeftByTransform();
+			}
+			if (_moveRight.IsPressed())
+			{
+				MoveRightByTransform();
+			}
+			if (_moveBack.IsPressed())
+			{
+				MoveBackByTransform();
+			}
+
+			if (_rotateLeft.IsPressed())
+			{
+				RotateLeftByTransform();
+			}
+			if (_rotateRight.IsPressed())
+			{
+				RotateRightByTransform();
+			}
+		}
+		
+		private void OnEnable()
+		{
+			_moveForward.Enable();
+			_moveLeft.Enable();
+			_moveRight.Enable();
+			_moveBack.Enable();
+            
+			_rotateLeft.Enable();
+			_rotateRight.Enable();
+		}
+
+		private void OnDisable()
+		{
+			_moveForward.Disable();
+			_moveLeft.Disable();
+			_moveRight.Disable();
+			_moveBack.Disable();
+            
+			_rotateLeft.Disable();
+			_rotateRight.Disable();
+		}
+
+		#endregion
+		
+		private void MoveForwardByTransform()
+		{
+			transform.Translate(Vector3.forward * (movementSpeed * Time.deltaTime));
+		}
+        
+		private void MoveLeftByTransform()
+		{
+			transform.Translate(Vector3.left * (movementSpeed * Time.deltaTime));
+		}
+        
+		private void MoveRightByTransform()
+		{
+			transform.Translate(Vector3.right * (movementSpeed * Time.deltaTime));
+		}
+        
+		private void MoveBackByTransform()
+		{
+			transform.Translate(Vector3.back * (movementSpeed * Time.deltaTime));
+		}
+        
+		private void RotateLeftByTransform()
+		{
+			var targetAngle = -90 * rotationSpeed * Time.deltaTime;
+			transform.Rotate(0, targetAngle, 0);
+		}
+        
+		private void RotateRightByTransform()
+		{
+			var targetAngle = 90 * rotationSpeed * Time.deltaTime;
+			transform.Rotate(0, targetAngle, 0);
+		}
+	}
+}
