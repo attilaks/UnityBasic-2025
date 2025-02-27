6612cfe3879aaea8fc0912ad60d26606dee164f0
Скотников Кирилл Андреевич <KASkotnikov@sberbank.ru>
2025-02-27 03:10:00 +0300

    basic shooting

--- Assets/Scripts/Tools/FirstPersonCamera.cs
+++ Assets/Scripts/Tools/FirstPersonCamera.cs
@@ -1,20 +1,14 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace Tools
 {
 	public class FirstPersonCamera : MonoBehaviour
 	{
 		[SerializeField] private float mouseSensitivity = 200f;
+		[SerializeField] private Transform playerTransform;
 		
-		[SerializeField] private Transform _playerBody;
 		private float _xRotation;
 		private float _yRotation;
-
-		private void Awake()
-		{
-			// _playerBody = gameObject.GetComponentInParent<Transform>();
-		}
 
 		private void Start()
 		{
@@ -25,14 +19,12 @@ namespace Tools
 		{
 			var mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
 			var mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
-
-			_playerBody.Rotate(Vector3.up * (mouseX * Time.deltaTime * mouseSensitivity));
 			
-			_xRotation -= mouseY;
+			_xRotation = Mathf.Clamp(_xRotation - mouseY, -90f, 90f);
 			_yRotation += mouseX;
-
+			
+			playerTransform.localRotation = Quaternion.Euler(0f, _yRotation, 0f);
 			transform.localRotation = Quaternion.Euler(_xRotation, 0f, 0f);
-			// _playerBody.Rotate(0, targetAngle, 0);
 		}
 	}
 }
---
+++ Assets/Scripts/Tools/Weapons/Pistol.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using GlobalConstants;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Tools.Weapons
+{
+	public class Pistol : MonoBehaviour
+	{
+		[SerializeField] private GameObject bulletPrefab;
+		[SerializeField] private Transform firePoint;
+		[SerializeField] private float fireRate;
+		[SerializeField] private float bulletForce;
+		
+		private float _nextFireTime;
+		
+		private readonly InputAction _shoot = new("Shoot", InputActionType.Button, 
+			$"{InputConstants.Mouse}/{InputConstants.LeftButton}");
+
+		private void OnEnable()
+		{
+			_shoot.performed += OnShootPerformed;
+			_shoot.Enable();
+		}
+
+		private void OnDisable()
+		{
+			_shoot.performed -= OnShootPerformed;
+			_shoot.Disable();
+		}
+		
+		private void OnShootPerformed(InputAction.CallbackContext context)
+		{
+			if (Time.time >= _nextFireTime)
+			{
+				Shoot();
+				_nextFireTime = Time.time + fireRate;
+			}
+		}
+
+		private void Shoot()
+		{
+			GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+			Rigidbody rb = bullet.GetComponent<Rigidbody>();
+			
+			rb.AddForce(firePoint.forward * bulletForce, ForceMode.Impulse);
+			
+			Destroy(bullet, 2f);
+		}
+	}
+}
