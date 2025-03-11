a172590fc2bd5dfdc558537687c0bc25c511b274
Скотников Кирилл Андреевич <KASkotnikov@sberbank.ru>
2025-03-11 22:12:41 +0300

    so in progress

--- Assets/ScriptableObjects/Weapon.cs
+++ Assets/ScriptableObjects/Weapon.cs
@@ -5,15 +5,24 @@ namespace ScriptableObjects
 	[CreateAssetMenu(menuName = "ScriptableObjects/Weapon", fileName = "NewWeapon")]
 	public class Weapon : ScriptableObject
 	{
-		[Tooltip("Name of the weapon")]
-		[SerializeField] private string weaponName;
-		[SerializeField] private int damage;
+		[Header("Prefab references")]
+		[Tooltip("Prefab of the bullet, rocket, etc.")]
+		[SerializeField] private GameObject bulletPrefab;
+		[SerializeField] private GameObject casingPrefab;
+		[SerializeField] private GameObject muzzleFlashPrefab;
+		
+		[Header("Location references")]
+		[SerializeField] private Transform casingExitLocation;
+		[SerializeField] private Transform firePoint;
+		
+		[Header("Settings")]
+		[Tooltip("Casing Ejection Speed")] 
+		[SerializeField] private float ejectPower = 150f;
 		[Tooltip("How fast the weapon can shoot")]
-		[SerializeField] private float fireRate;
+		[SerializeField] private float fireRate = 0.14f;
 		[Tooltip("How far the weapon can shoot")] 
-		[SerializeField] private float fireRange;
+		[SerializeField] private float fireRange = 100f;
 		[Tooltip("Force with which bullets fly out of weapon")]
-		[SerializeField] private float bulletForce;
-		[SerializeField] private GameObject projectilePrefab;
+		[SerializeField] private float bulletForce = 150;
 	}
 }
