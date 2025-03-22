c90122b12967adbe591e1b395bcb1c716b458160
Скотников Кирилл Андреевич <KASkotnikov@sberbank.ru>
2025-03-22 08:14:11 +0300

    made sound sliders

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -1,4 +1,5 @@
-﻿using TMPro;
+﻿using System;
+using TMPro;
 using Tools.Weapons;
 using UnityEngine;
 using UnityEngine.UI;
@@ -19,11 +20,15 @@ namespace UI
 		[SerializeField] private TextMeshProUGUI healthText;
 		
 		[Header("Settings references")]
-		[SerializeField] private Scrollbar soundVolumeSlider;
+		[SerializeField] private Slider soundVolumeSlider;
+		[SerializeField] private Slider musicVolumeSlider;
 		[SerializeField] private Button exitButton;
 		[SerializeField] private GameObject settingsMenu;
 		
 		private bool _isMenuOpen;
+		
+		public static event Action<float> SoundEffectsSliderValueChanged = delegate { };
+		public static event Action<float> MusicSliderValueChanged = delegate { };
 		
 		private void Awake()
 		{
@@ -31,6 +36,8 @@ namespace UI
 			weaponController.CurrentWeaponAmmoCountChanged += OnCurrentWeaponAmmoCountChanged;
 			
 			exitButton.onClick.AddListener(OnExitButtonClicked);
+			soundVolumeSlider.onValueChanged.AddListener(OnSoundEffectsSliderValueChanged);
+			musicVolumeSlider.onValueChanged.AddListener(OnMusicSliderValueChanged);
 		}
 
 		private void OnDestroy()
@@ -39,6 +46,8 @@ namespace UI
 			weaponController.CurrentWeaponAmmoCountChanged -= OnCurrentWeaponAmmoCountChanged;
 			
 			exitButton.onClick.RemoveListener(OnExitButtonClicked);
+			soundVolumeSlider.onValueChanged.RemoveListener(OnSoundEffectsSliderValueChanged);
+			musicVolumeSlider.onValueChanged.RemoveListener(OnMusicSliderValueChanged);
 		}
 
 		private void Update()
@@ -98,6 +107,16 @@ namespace UI
 			#if UNITY_EDITOR
 			UnityEditor.EditorApplication.isPlaying = false;
 			#endif
+		}
+		
+		private void OnSoundEffectsSliderValueChanged(float volume)
+		{
+			SoundEffectsSliderValueChanged.Invoke(volume);
+		}
+		
+		private void OnMusicSliderValueChanged(float volume)
+		{
+			MusicSliderValueChanged.Invoke(volume);
 		}
 	}
 }
