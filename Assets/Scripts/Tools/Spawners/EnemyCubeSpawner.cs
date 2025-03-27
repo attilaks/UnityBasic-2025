9842e79f74e1e9b7ecbf19b4382fa92212a1055f
Скотников Кирилл Андреевич <KASkotnikov@sberbank.ru>
2025-03-28 02:13:26 +0300

    rolled back EnemyCubeSpawner.cs

--- Assets/Scripts/Tools/Spawners/EnemyCubeSpawner.cs
+++ Assets/Scripts/Tools/Spawners/EnemyCubeSpawner.cs
@@ -1,68 +1,44 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using Characters;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace Tools.Spawners
 {
+	[RequireComponent(typeof(Collider))]
 	public class EnemyCubeSpawner : MonoBehaviour
 	{
 		[SerializeField] private EnemyCube enemyCubePrefab;
-		[SerializeField] private EnemySpawnLocationSetType enemySpawnLocationSetType;
-		[SerializeField] private byte maxEnemyCount;
-		[SerializeField] private float maxSpawnInterval;
 		
 		private Vector2 _groundSize;
 		private EnemyCube _currentEnemyCube;
-		private float _lastEnemySpawnTimeDelta;
-
-		private byte _enemiesOnSceneCount;
-		private Transform[] _spawnPoints;
-		private Dictionary<Transform, bool> _canSpawn = new();
-		private Dictionary<EnemyCube, bool> _canDestroy = new();
 		
 		private void Awake()
 		{
-			_spawnPoints = gameObject.GetComponentsInChildren<Transform>();
+			var groundCollider = GetComponent<Collider>();
+			_groundSize = new Vector2(groundCollider.bounds.size.x, groundCollider.bounds.size.z);
 		}
 		
 		private void Start()
 		{
-			if (TryGetComponent<Collider>(out var groundCollider))
-			{
-				_groundSize = new Vector2(groundCollider.bounds.size.x, groundCollider.bounds.size.z);
-				SpawnEnemyCube();
-			}
-		}
-		
-		private void Update() 
-		{
-			_lastEnemySpawnTimeDelta += Time.deltaTime;
-
-			if (_lastEnemySpawnTimeDelta > maxSpawnInterval && _enemiesOnSceneCount < maxEnemyCount)
-			{
-				SpawnEnemyCube();
-			}
+			SpawnEnemyCube();
 		}
 
 		private void SpawnEnemyCube()
 		{
-			Vector3 randomPosition = new Vector3(
+			var randomPosition= new Vector3(
 				Random.Range(-_groundSize.x / 2, _groundSize.x / 2),
 				0.5f,
 				Random.Range(-_groundSize.y / 2, _groundSize.y / 2)
 			);
-
+			
 			if (_currentEnemyCube)
 				_currentEnemyCube.OnDeath -= SpawnEnemyCube;
 			
 			_currentEnemyCube = Instantiate(enemyCubePrefab, randomPosition, Quaternion.identity);
 			_currentEnemyCube.OnDeath += SpawnEnemyCube;
-		}
-
-		private enum EnemySpawnLocationSetType
-		{
-			FullyRandom = 0,
-			InPreparedSpawnPoints = 1
 		}
 	}
 }
