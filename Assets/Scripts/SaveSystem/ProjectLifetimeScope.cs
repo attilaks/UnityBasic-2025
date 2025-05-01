134c7c5638e738ddb2d1704259c37f2365ccb1c9
Скотников Кирилл Андреевич <KASkotnikov@sberbank.ru>
2025-05-01 20:55:33 +0300

    shortened ProjectLifetimeScope

--- Assets/Scripts/SaveSystem/ProjectLifetimeScope.cs
+++ Assets/Scripts/SaveSystem/ProjectLifetimeScope.cs
@@ -1,29 +1,13 @@
-﻿using Tools.Managers;
-using Tools.Managers.Interfaces;
-using VContainer;
+﻿using VContainer;
 using VContainer.Unity;
 
 namespace SaveSystem
 {
 	public class ProjectLifetimeScope : LifetimeScope
 	{
-		private void RegisterPersistentDependencies(IContainerBuilder builder)
-		{
-			builder.Register<ISaveService, JsonSaveService>(Lifetime.Singleton);
-			
-			// builder.RegisterComponentInHierarchy<SaveLoadManager>().DontDestroyOnLoad();
-		}
-
-		private void RegisterSceneDependencies(IContainerBuilder builder)
-		{
-			builder.RegisterComponentInHierarchy<FirstPersonMovementManager>().As<IPlayerTransformReader>();
-			builder.RegisterComponentInHierarchy<FirstPersonCamera>().As<ICameraReader>();
-			// builder.RegisterComponentInHierarchy<SaveLoadManager>();
-		}
-
 		protected override void Configure(IContainerBuilder builder)
 		{
-			RegisterPersistentDependencies(builder);
+			builder.Register<ISaveService, JsonSaveService>(Lifetime.Singleton);
 		}
 	}
 }
